ed550256e820bd97ee5600105cc829e4736e95a9
Yuya Aratani <hrgruri@gmail.com>
2016-06-03 15:14:17 +0900

    books/detailの追加

--- lib/Controller/Detail.php
+++ lib/Controller/Detail.php
@@ -1,22 +1,29 @@
 <?php
 namespace Hrgruri\Icd3\Controller;
 
-use Hrgruri\Icd3\Model\Nishikie;
+use Hrgruri\Icd3\Model\{Nishikie, Books};
 use Hrgruri\Icd3\Model\Recommend;
 
 class Detail
 {
     public static function show($args, $twig)
     {
-        $html = 'error';
-        if ($args['db'] == 'nishikie') {
-            $info       = Nishikie::getDetail($args['id']);
+        try {
+            if ($args['db'] == 'nishikie') {
+                $info       = Nishikie::getDetail($args['id']);
+            } elseif ($args['db'] == 'books') {
+                $info       = Books::getDetail($args['id']);
+            } else {
+                throw new \Exception();
+            }
             $recommend  = Recommend::getByDetail($args['db'], $args['id'], 4);
             $html = ($twig->loadTemplate('detail.twig'))->render([
                 'title'     => '浮世絵データベース',
                 'info'      => $info,
                 'recommend' => $recommend
             ]);
+        } catch(\Exception $e) {
+            $html = 'error';
         }
         return $html;
     }
